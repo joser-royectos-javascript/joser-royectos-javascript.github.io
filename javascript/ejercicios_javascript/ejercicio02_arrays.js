088a0fe0b291cae91d223b8fc6d5b243a4c5f148
joser-royectos-javascript <156824889+joser-royectos-javascript@users.noreply.github.com>
2024-01-19 17:25:52 +0100

    ejemplo

--- javascript/ejercicios_javascript/ejercicio02_arrays.js
+++ javascript/ejercicios_javascript/ejercicio02_arrays.js
@@ -11,25 +11,13 @@ let mascotas = ['perrito.jpg','gatito.avi','loro.mp3', 'galápago.docx', 'gallo.
 
 console.log(mascotas);
 
+//Opción 2 - Función arrow
+let mascoEjpg = mascotas.filter(masco => {
+    return masco.toUpperCase().includes('JPG');
+});
 
-/*
-let ficheros = ["Alumno 1","Alumno 2","Alumno 3","Alumno 4"];
+console.log(mascoEjpg);
 
-let dosPrimeros = ficheros.slice(0,2);
-console.log(dosPrimeros);
-rodaja = ficheros.slice(2,);//Desde la posición 2 hasta el final
-console.log(rodaja);
-rodaja = ficheros.slice();//Todos
-console.log(rodaja);
-rodaja = ficheros.slice(undefined,3);//Los tres primeros
-console.log(rodaja);
+let posPunto = mascoEjpg.lastIndexOf(".");
+console.log(mascoEjpg.slice(0,posPunto));
 
-
-let fichero = "selfie.jpg";
-let posicionPunto = fichero.lastIndexOf(".");
-console.log(fichero.slice(posicionPunto+1,));//jpg
-console.log(fichero.slice(0,posicionPunto));//selfie
-
-console.log(fichero.slice(-3,))//jpg --> Funcionará siempre que la extensión tenga 3 caracteres
-
-*/
