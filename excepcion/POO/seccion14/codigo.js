0546a27d3f477b17a11240f941afe1d5ae9c34af
joser-royectos-javascript <156824889+joser-royectos-javascript@users.noreply.github.com>
2024-01-29 17:06:09 +0100

    class

---
+++ excepcion/POO/seccion14/codigo.js
@@ -0,0 +1,9 @@
+const MINIMO =5;
+const OPTIMO =8;
+document.querySelector("identificador_1").addEventListener("input",()=>{
+    const idNombre = document.querySelector("identificador_1").Value;
+    const nombre = idNombre.Value;
+    if (nombre.trim().length < MINIMO){
+        
+    }
+})
