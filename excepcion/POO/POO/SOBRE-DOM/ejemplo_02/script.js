a591c081a2d06eb32a074c9b2bc85d8bdb49f260
joser-royectos-javascript <156824889+joser-royectos-javascript@users.noreply.github.com>
2024-02-01 16:27:13 +0100

    commint

---
+++ excepcion/POO/POO/SOBRE-DOM/ejemplo_02/script.js
@@ -0,0 +1,80 @@
+*{
+    margin:3px;
+    padding: 0;
+    box-sizing: border-box;
+}
+body{
+    height: 95vh;
+    background: black;
+    color: rgb(164, 207, 207);
+    font-family:sans-serif;
+    font-size: 14px;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+}
+header{
+    border-radius: 15px;
+    background: rgb(20, 20, 20);
+    font-size: 24px;
+    color: rgb(255, 255, 255);
+    padding: 2em 0 2em 0;
+    position:relative;
+    top: 0;
+    position: sticky;
+    transition: width 0.5s;
+}
+header:hover {
+    color: rgb(224, 224, 224);
+}
+.principal,footer{
+    display: flex;
+    flex: 1;
+}
+.izquierda{
+    background: rgb(44, 38, 38);
+    font-size: 14px;
+    color: rgb(239, 233, 233);
+    padding: 3em 0 3em 0;  
+    flex: 1 1 100px; 
+}
+.titulo{margin-bottom: 10%;}
+main{
+    background: rgb(44, 38, 38);
+    font-size: 14px;
+    color: rgb(239, 233, 233);
+    padding: 3em 0 3em 0;
+    flex: 5 5 150px;    
+     
+}
+.derecha{
+    background: rgb(44, 38, 38);
+    font-size: 14px;
+    color: rgb(239, 233, 233);
+    padding: 1em 0 1em 0; 
+    flex: 1 1 100px;   
+}
+footer{
+    background: rgb(11, 11, 11);
+    font-size: 14px;
+    color: rgb(140, 129, 129);
+    padding: 1em 0 1em 0; 
+    position: sticky;
+    transition: width 0.5s;
+}
+aside {
+    border: 2px solid rgb(26, 29, 79);
+    border-radius: 12px;}
+article {
+    padding-bottom: 15%vh;
+    border: 2px solid rgb(118, 118, 149);
+    border-radius: 12px;}
+/* Media query */
+@media (max-width: 500px) {
+    .principal {
+        flex-direction: column;
+    }
+    main {
+        padding: 7em 0;
+    }
+}
